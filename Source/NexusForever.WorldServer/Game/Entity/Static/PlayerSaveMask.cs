a4a101a02b01eba57ee803988423b6fac84013e6
Kirmmin <kirmmin@hotmail.com>
2019-06-26 17:14:39 -0700

    Feature: Appearance Change Vendor working; Includes fixes for character appearance
    Character Appearance changing finalised
    
    Ported Customisation models to classes
    Change update procedure to only update changed or removed values

--- Source/NexusForever.WorldServer/Game/Entity/Static/PlayerSaveMask.cs
+++ Source/NexusForever.WorldServer/Game/Entity/Static/PlayerSaveMask.cs
@@ -17,6 +17,7 @@ namespace NexusForever.WorldServer.Game.Entity.Static
         Affiliation = 0x0020,
         Holomark    = 0x0040,
         Innate      = 0x0080,
-        BindPoint   = 0x0100
+        BindPoint   = 0x0100,
+        Appearance  = 0x0200,
     }
 }
