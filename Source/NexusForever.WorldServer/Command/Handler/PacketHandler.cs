ffd8825c8767b5499f207f712a88d1aa269bcb3d
Kirmmin <kirmmin@hotmail.com>
2019-07-02 06:55:00 -0700

    Command updates

---
+++ Source/NexusForever.WorldServer/Command/Handler/PacketHandler.cs
@@ -0,0 +1,34 @@
+﻿using NLog;
+using NexusForever.WorldServer.Command.Attributes;
+using NexusForever.WorldServer.Command.Contexts;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using NexusForever.WorldServer.Network.Message.Model.Shared;
+
+namespace NexusForever.WorldServer.Command.Handler
+{
+    [Name("Packet")]
+    public class PacketHandler : NamedCommand
+    {
+        private static readonly ILogger log = LogManager.GetCurrentClassLogger();
+
+        public PacketHandler()
+            : base(true, "packet")
+        {
+        }
+
+        protected override Task HandleCommandAsync(CommandContext context, string command, string[] parameters)
+        {
+            log.Info($"{parameters.Length}");
+
+            foreach (string packet in parameters)
+            {
+                string[] split = packet.Split(':');
+                context.Session.EnqueueMessageEncrypted((uint)Int32.Parse(split[0]), split[1]);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
