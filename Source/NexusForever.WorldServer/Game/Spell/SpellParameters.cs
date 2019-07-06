56b8f70256198bf7b8e3ef90ad622764d7552bcb
Kirmmin <kirmmin@hotmail.com>
2019-07-06 13:13:30 -0700

    Implemented property modifiers via spell effect
    Added mount speed to DB script
    
    Remove debug log in AssetManager
    
    Remove debug logging on Player
    
    Added basic calculation for Stack applications
    Tidied up indentation
    
    Health stats added to WorldEntity

--- Source/NexusForever.WorldServer/Game/Spell/SpellParameters.cs
+++ Source/NexusForever.WorldServer/Game/Spell/SpellParameters.cs
@@ -7,7 +7,7 @@ namespace NexusForever.WorldServer.Game.Spell
         public SpellInfo SpellInfo { get; set; }
         public SpellInfo ParentSpellInfo { get; set; }
         public SpellInfo RootSpellInfo { get; set; }
-        public bool UserInitiatedSpellCast { get; set; }
+        public bool UserInitiatedSpellCast { get; set; } = true;
         public uint PrimaryTargetId { get; set; }
         public Position Position { get; set; }
         public ushort TaxiNode { get; set; }
